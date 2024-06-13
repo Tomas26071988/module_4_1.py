d4056146fd88e25a8d2f9203ecef9965ebd5554a
Tomas26071988 <artem_1988_artem1988@icloud.com>
2024-06-13 22:58:20 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,11 @@
+def divide_(a, b):
+    if b == 0:
+        return str('Ошибка')
+    else:
+        return a/b
+
+
+
+
+
+
---
+++ true_math.py
@@ -0,0 +1,8 @@
+from math import inf
+
+def divide_(a, b):
+    if b == 0:
+        return inf
+    else:
+        return a/b
+
